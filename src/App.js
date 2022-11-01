ea51ba1c6bbe301f91cd793f077fe0afa5f4a444
Temz <temitopeabiodun685@gmail.com>
2022-11-01 13:10:25 +0100

    added border countries

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.scss';
 import { CountryData } from './CountryData';
 import {CountryFullDetails} from './CountryFullDetails'
 import axios from 'axios';
-import PropagateLoader from "react-spinners/PropagateLoader";
+import ClipLoader from "react-spinners/ClipLoader";
 import { useEffect, useState } from 'react';
 import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import {Search} from './Search';
 
-function App(props) {
+function App() {
 
   const [data,setData]=useState([]);   
   const [loading, setLoading]=useState(false);
@@ -116,10 +116,10 @@ function App(props) {
               loading?
         
               <div className='loader'>
-                 <PropagateLoader color={'#36d7b7'} size={10}/><br/>
+                 <ClipLoader color={'#36d7b7'} size={100}/><br/>
               </div>
              :
-              data.map((i,index)=>{
+              data.map((i)=>{
                 return(
                   <CountryData key={i.name.common}
                   divId={i.name.common} 
@@ -137,9 +137,10 @@ function App(props) {
 
           </>              
         }/>
-        <Route path='/country_details' element={
-          <CountryFullDetails/>
-        }/>     
+          <Route path='/country_details' element={
+            <CountryFullDetails/>
+          }/>
+             
       </Routes>
     </Router>
   );
@@ -147,163 +148,6 @@ function App(props) {
 
 export default App;
 
-// import './App.scss';
-// import { CountryData } from './CountryData';
-// import axios from 'axios';
-// import PropagateLoader from "react-spinners/PropagateLoader";
-// import { useEffect, useState } from 'react';
-// import { CountryFullDetails } from './CountryFullDetails';
-// import {BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-// import {Search} from './Search';
-// function App() {
-//   const [data,setData]=useState([]);  
-//   const [loading, setLoading]=useState(false);
-//   const [country, setCountry] = useState('');
-//   const [region, setRegion]=useState('');
-//   const [track, setTrack]=useState({
-//     name:{'common':''},
-//     population:'',
-//     region:'',
-//     capital:'',
-//     flags:{png:''},
-//     subregion:'',
-//     timezones:[],
-//     continents:[],
-//   })
-
-//   //load all countries on first mount
-//   useEffect(()=>{
-//     setLoading(true);
-//       const arr =[];   
-//         axios.get('https://restcountries.com/v3.1/all')
-//           .then(res=>{     
-//             for(let i=0;i<res.data.length;i++){
-//               arr.push(res.data[i]);
-//             }
-//             setData(()=>arr);
-//             setLoading(false);
-//           })
-//           .catch(err=>{
-//             setLoading(false);
-//             // document.getElementsByClassName('error')[0].innerHTML = err.message;
-//           })
-//   },[region, country]);
-
-//   //search by region
-//   useEffect(()=>{
-//     setLoading(true);
-//     const arr =[];
-//     axios.get(`https://restcountries.com/v3.1/region/${region}`)
-//     .then(res=>{
-//       for(let i=0;i<res.data.length;i++){
-//         arr.push(res.data[i]);
-//       }
-//       setData(()=>arr);
-//       setLoading(false);
-//     })
-//   },[region]);
-
-//   //search all countries
-//   useEffect(()=>{
-//     setLoading(true);
-//     const arr =[];
-//     axios.get(`https://restcountries.com/v3.1/name/${country}`)
-//     .then(res=>{
-//       for(let i=0;i<res.data.length;i++){
-//         arr.push(res.data[i]);
-//       }
-//       setData(()=>arr);    
-//       setLoading(false);
-//     })
-//   },[country]);
-
-//   // useEffect(()=>{
-//   //   setLoading(true);
-//   //   axios.get(`https://restcountries.com/v3.1/name/all`)
-//   //   .then(res=>{
-//   //     for(let i=0;i<res.data.length;i++){
-//   //       // const a = document.getElementsByClassName('countryData')
-//   //       console.log(res.data[i])
-//   //     }
-//   //   })
-//   // },[])
-//   //control the search input
-//   const handleChange=(e)=>{
-//     setCountry((data)=>{
-//       return data=e.target.value;
-//     })
-//   }
-
-//   //set the continent value on select
-//   const handleSelect=()=>{
-//     const continent = document.getElementById('filter').value;
-//     setRegion(continent);
-//   }
-
-  
-//  const nextPage=(e)=>{
-//   setTrack((obj)=> obj=e)
-//   console.log(e)
-//  }
- 
-//   return (
-//     <Router>
-//       <div className="App">
-//           <header>
-//             <div className='logo'><h3>Where in the world</h3></div>
-//             <div className='darkMode'><h5>Dark Mode</h5></div>
-//           </header>
-//           <br/>
-//       </div>
-
-//       <Routes>
-        
-//         <Route path='/' element={              
-//           <>
-//             <Search search={country} changeSearch={handleChange} select={handleSelect}/>  
-
-//             {
-//               loading?
-        
-//               <div className='loader'>
-//                  <PropagateLoader color={'#36d7b7'} size={10}/><br/>
-//               </div>
-//              :
-//               data.map(i=>{
-//                 return(
-//                   <CountryData key={i.name.common}
-//                   divId={i.name.common} 
-//                   img={i.flags.png}
-//                   alt={i.name.common}
-//                   country={i.name.common}
-//                   population={i.population}
-//                   region={i.region}
-//                   capital={i.capital}
-//                   handleClick={()=>nextPage(i)}/> 
-//                 )
-//               })
-//             }
-
-//           </>              
-//         }/>
-//         <Route path='/country_details' element={
-//           <CountryFullDetails
-//           img={track.flags.png}
-//           country={track.name.common}
-//           population={track.population}
-//           region={track.region}
-//           capital={track.capital}
-//           subregion={track.subregion}
-//           continent={track.continents[0]}
-//           timezone={track.timezones[0]}
-//           />
-//         }/>       
-//       </Routes>
-//     </Router>
-//   );
-// }
-
-// export default App;
 
 
  
--- src/CountryFullDetails.js
+++ src/CountryFullDetails.js
@@ -5,31 +5,31 @@ import { useEffect } from 'react';
 import { useState } from 'react';
 
 export const CountryFullDetails = (props) => {
+    
     const [newData, setNewData] = useState({
         name:{'common':''},
-            population:'',
-            region:'',
-            capital:'',
-            flags:{png:''},
-            subregion:'',
-        //     timezones:[],
-            continents:[],
-            currencies:{obj:{name:''}},
-            languages:{obj:{eng:''}},
+        population:'',
+        region:'',
+        capital:'',
+        flags:{png:''},
+        subregion:'',
+        timezones:[],
+        continents:[],
+        currencies:{obj:{name:''}},
+        languages:{obj:{eng:''}},
+        borders:[]
     });
-    const [storage, setStorage] = useState([]);
 
     useEffect(()=>{
         let isCleaning = false; 
         const save =  JSON.parse(localStorage.getItem('dataIndex'));                
             if(!isCleaning){
                 setNewData(()=>save[0]);
-                setStorage(()=>save[0])
             } 
             return ()=>{
             isCleaning = true; 
             }
-      },[storage]);
+    },[]);
 
   return (
     <div className='moreInfo'>
@@ -38,25 +38,41 @@ export const CountryFullDetails = (props) => {
                 <input type='button' value='Back'/>
             </div>
         </Link>
-
+        <br/>
+        <br/>
         <div>
             <div>
                 <img src={newData.flags.png} width={'100%'} height={'100%'} alt={props.flag}/>
             </div>
-            <div className='info'>
-                <div><h3>Country: {newData.name.common}</h3></div>
+            <div className='infoBack'>
+                <div><h2>{newData.name.common}</h2></div>
+                <div><p>Capital: {newData.capital}</p></div>
+                <div><p>Continent: {newData.continents[0]}</p></div>
                 <div><p>Population: {newData.population}</p></div>
                 <div><p>Region: {newData.region}</p></div>
-                <div><p>Capital: {newData.capital}</p></div>
                 <div><p>Sub Region: {newData.subregion}</p></div>
             </div>
+            <br/>
 
-            <div>
-                <div><p>continent: {newData.continents[0]}</p></div>
+            <div className='infoBack'>
+                <div><p>Time-zone: {newData.timezones[0]}</p></div>
                 <div><p>Currencies:{` ${Object.keys(newData.currencies)}`}</p></div>
-                <div><p>languages: {` ${Object.values(newData.languages)}`}</p></div>
+                <div><p>Languages: {` ${Object.values(newData.languages)}`}</p></div>
             </div>
-            
+            <br/>
+            <div className='border'>
+                {
+                    //if border is not present in the data 
+                    newData.borders === undefined ?
+                    <span></span>:
+                    <>
+                        <div className='first'>{newData.borders[0]}</div>
+                        <div>{newData.borders[1]}</div>
+                        <div>{newData.borders[2]}</div>
+                    </>
+                }
+            </div>
+            <br/>
         </div>
     </div>
   )
